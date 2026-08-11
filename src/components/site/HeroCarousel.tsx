62eafd88915d14a45784f258ed6758c7010ea16c
Zuhayr Smith <suhairsmith17@gmail.com>
2026-08-11 21:38:57 +0200

    new banner

--- src/components/site/HeroCarousel.tsx
+++ src/components/site/HeroCarousel.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import slide1 from "@/assets/hero1.jpeg";
+import slide1 from "@/assets/babas-and-brasse-banner-new.jpg";
 import slide1Mobile from "@/assets/hero-slide-1-mobile.png";
 import slide2 from "@/assets/hero2.webp";
 import slide3 from "@/assets/hero3.webp";
